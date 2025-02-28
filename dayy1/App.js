c8f51ec7a6f8ccd797a517778dd88ac3a75fd7de
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-28 19:39:56 +0530

    Add files via upload

---
+++ dayy1/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import {Comp3} from './Comp3';
+import Comp4 from './Comp4';
+import Comp5 from './Comp5';
+import Comp6 from './Comp6';
+import Comp7 from './Comp7';
+import C1 from './C1';
+import C3 from './C3';
+function App() {
+  return (
+    <div className="App">
+   <Comp3 />
+   <br></br>
+   < Comp4 />
+   <Comp6 />
+   <C3 />
+
+   <C1/>
+   
+
+   <Comp5/>
+    </div>
+  );
+}
+
+export default App;
