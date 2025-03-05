9510049e46652fdd76267817fe9f3fed5361f918
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-03-05 09:56:50 +0530

    Add files via upload

---
+++ dayy2/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Product from './Product1';
+import Product1 from './Product2';
+import Product2 from './Product3';
+
+function App() {
+  return (
+    <div>
+  <Product />
+  <Product1 />
+  <Product2 />
+  </div>
+  );
+}
+
+export default App;
