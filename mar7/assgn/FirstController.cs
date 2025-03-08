3687fc5416db88ddf5bb4143c698f74f621dea47
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-03-08 18:00:40 +0530

    Add files via upload

---
+++ mar7/assgn/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return Content("FirstController - Index1");
+        }
+
+        public IActionResult Index2()
+        {
+            return Content("FirstController - Index2");
+        }
+
+        public IActionResult Index3()
+        {
+            return Content("FirstController - Index3");
+        }
+    }
+}
---
+++ mar7/assgn/SecondController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo1.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return Content("SecondController - Index1");
+        }
+
+        public IActionResult Index2()
+        {
+            return Content("SecondController - Index2");
+        }
+
+        public IActionResult Index3()
+        {
+            return Content("SecondController - Index3");
+        }
+    }
+}
